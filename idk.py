4881b43bcc79afb1b20ad1bacc184d7537f90831
kusti420 <lilleo@ttu.ee>
2022-03-18 16:54:23 +0200

    made idk.py add channel name as well and changed playlist.csv

--- idk.py
+++ idk.py
@@ -6,6 +6,7 @@ import csv
 def get_links_from_playlist(link: str, developer_key: str) -> list:
     links = []
     titles = []
+    channel_names = [] # name of the channel who uploaded that video
     youtube = build('youtube', 'v3', developerKey=developer_key)
     request = youtube.playlistItems().list(
         part='snippet',
@@ -22,13 +23,18 @@ def get_links_from_playlist(link: str, developer_key: str) -> list:
                 for item in response['items']:
                     links.append(item['snippet']['resourceId']['videoId'])
                     titles.append(item['snippet']['title'])
+                    try:
+                        channel_names.append(item['snippet']['videoOwnerChannelTitle'])
+                    except KeyError:
+                        channel_names.append('removed?')
                     print(len(links))
                 request = youtube.playlistItems().list_next(request, response)
         except AttributeError:
             links = [f"https://www.youtube.com/watch?v={link}" for link in links]
             links = reversed(links)
             titles = reversed(titles)
-            return zip(titles, links)
+            channel_names = reversed(channel_names)
+            return zip(titles, links, channel_names)
 
 
 if __name__ == '__main__':
@@ -38,14 +44,15 @@ if __name__ == '__main__':
     playlist = get_links_from_playlist(URL, KEY)
     # print(*playlist, sep='\n')
     links_in_csv = []
+    channel_names_in_csv = []
     with open("playlist.csv", "r", encoding="utf-8", newline="") as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
-        for title, link in reader:
+        for title, link, channel_name in reader:
             links_in_csv.append(link)
         csvfile.close()
     with open("playlist.csv", "a", encoding="utf-8", newline="") as csvfile:
         writer = csv.writer(csvfile, delimiter=',')
-        for title, link in playlist:
+        for title, link, channel_name in playlist:
             if link not in links_in_csv:
-                writer.writerow([title, link])
+                writer.writerow([title, link, channel_name])
     pass
