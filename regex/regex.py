86239f6d952bcd8cdcbe663cbe56b43997bdc0ae
kusti420 <lilleo@ttu.ee>
2022-03-29 14:01:48 +0300

    nvm now its fixed

--- regex/regex.py
+++ regex/regex.py
@@ -11,7 +11,7 @@ for folder in folders:
     for file in files:
         if re.search(rf"{a}(.*)", file) != None:
             pattern_a[file] = re.search(rf"{a}(.*)", file).group(1)
-        if re.search(rf"{b}(.*)", file) != None:
+        elif re.search(rf"{b}(.*)", file) != None:
             pattern_b[file] = re.search(rf"{b}(.*)", file).group(1)
     [os.rename(path + folder + "/" + k, path + folder + "/" + v) for k, v in pattern_a.items()]
     [os.rename(path + folder + "/" + k, path + folder + "/" + v) for k, v in pattern_b.items()]
@@ -19,7 +19,7 @@ for folder in folders:
     for file in files:
         if re.search(rf"(.*){c}(.*)", file) != None:
             pattern_c[file] = re.search(rf"(.*){c}(.*)", file).group(1) + re.search(rf"(.*){c}(.*)", file).group(2)
-        if re.search(rf"(.*){d}(.*)", file) != None:
+        elif re.search(rf"(.*){d}(.*)", file) != None:
             pattern_d[file] = re.search(rf"(.*){d}(.*)", file).group(1) + re.search(rf"(.*){d}(.*)", file).group(2)
     [os.rename(path + folder + "/" + k, path + folder + "/" + v) for k, v in pattern_c.items()]
     [os.rename(path + folder + "/" + k, path + folder + "/" + v) for k, v in pattern_d.items()]
