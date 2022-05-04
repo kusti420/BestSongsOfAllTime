9e08f660ce4cf4ef3b74fa8f04f53d0c5475c852
kusti420 <kristjan0@live.com>
2022-05-04 21:32:06 +0300

    fixed delimiter issue

--- CSV/reverse.py
+++ CSV/reverse.py
@@ -6,11 +6,11 @@ import csv
 # save the reversed playlist.csv file
 
 with open("playlist.csv", "r", encoding="utf-8") as csvfile:
-    reader = csv.reader(csvfile, delimiter='■')
+    reader = csv.reader(csvfile, delimiter='|||')
     playlist = list(reader)
     playlist.reverse()
     # save the reversed playlist.csv file
 with open("playlist.csv", "w", encoding="utf-8", newline="") as csvfile:
-    writer = csv.writer(csvfile, delimiter='■')
+    writer = csv.writer(csvfile, delimiter='|||')
     for title, link in playlist:
         writer.writerow([title, link])
