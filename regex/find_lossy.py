4a665ba1e8c2774f696cbb32a43b801fb8647ad9
kusti420 <kristjan0@live.com>
2022-08-27 12:13:42 +0300

    idk

--- regex/find_lossy.py
+++ regex/find_lossy.py
@@ -13,5 +13,5 @@ for folder in folders:
         for file in files_in_folder:
             if file.endswith(".mp3") or file.endswith(".m4a"):
                 folders_with_lossy_files.add(folder)
-folders_with_lossy_files = [f"part{part_nr}" for part_nr in sorted([int(folder[folder.index("t")+1:]) for folder in folders_with_lossy_files])]
+folders_with_lossy_files = [f"part{part_nr}" for part_nr in sorted([int(folder[folder.index("part")+4:]) for folder in folders_with_lossy_files])]
 print(*folders_with_lossy_files, sep='\n')
