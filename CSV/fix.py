72d269cc0396a099eefd4106a083950e69ff0e08
kusti420 <kristjan0@live.com>
2022-10-25 23:15:56 +0300

    idk

--- CSV/fix.py
+++ CSV/fix.py
@@ -15,15 +15,24 @@ for i in range(len(lines)):
         lines[i] = lines[i][:-2]+","+lines[i][:-2]+","+lines[i]
     elif re.match(r"\d+-\d+,\d+-\d+,\d+-\d+", lines[i]):
         last_index = i + 1
-        last_part += 1
+        last_part = int(lines[i].split(",")[0].split("-")[1])
     elif len(lines[i].split(",")) == 2:
         lines[i] = lines[i][:-2]+",False"+lines[i][-2:]
-
+special_case = False
 for i in range(len(lines)):
-    if i - 45 == last_index:
+    try:
+        # print(any([re.match(r"\d+-\d+,\d+-\d+,\d+-\d+", lines[j]) for j in range(i, i+10)]))
+        if i - 45 >= last_index and any([re.match(r"\d+-\d+,\d+-\d+,\d+-\d+", lines[j]) for j in range(i, i+10)]):
+            special_case = True
+    except IndexError:
+        break
+    if i - 45 >= last_index and not special_case:
         lines = lines[:i] + [f"{last_part}-{last_part + 1},{last_part}-{last_part + 1},{last_part}-{last_part + 1}\r\n"] + lines[i:]
         last_index = i + 1
         last_part += 1
+    if i - 45 >= last_index and special_case:
+        if re.match(r"\d+-\d+,\d+-\d+,\d+-\d+", lines[i]):
+            special_case = False
 
 with open("downloaded_playlist.csv", "w", encoding="utf-8", newline="") as f:
     f.writelines(lines)
