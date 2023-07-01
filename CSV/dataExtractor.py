a12f845138f940502a5e2a959e760c1963694eb2
kusti420 <kristjan0@live.com>
2023-07-01 21:10:23 +0300

    cleanup

--- CSV/dataExtractor.py
+++ CSV/dataExtractor.py
@@ -5,6 +5,8 @@ import csv
 import os
 import datetime
 from types import SimpleNamespace
+import concurrent.futures
+
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
 with open("key.txt", "r") as key:
@@ -12,7 +14,6 @@ with open("key.txt", "r") as key:
     key.close()
 
 def get_playlist_data(link: str, developer_key: str = KEY) -> list:
-    # must get links, titles, channel names, date added to playlist, date added to youtube
     lst = []
     youtube = build('youtube', 'v3', developerKey=developer_key)
     request = youtube.playlistItems().list(
@@ -74,28 +75,15 @@ if __name__ == '__main__':
     master_playlist = get_playlist_data(URL)
     playlist = [item for item in master_playlist if item.link not in [itm.link.split("=")[-1] for itm in pl_csv]]
 
-    # TODO: make this part run multithreaded with parallellism
-    # for i, item in enumerate(playlist):
-    #     # print(item.date_added_to_youtube)
-    #     if not item.date_added_to_youtube:
-    #         playlist[i] = get_date_added_to_youtube(item)
-    #         print(i)
-
-    import concurrent.futures
 
     def process_item(item):
         if not item.date_added_to_youtube:
             return get_date_added_to_youtube(item)
         return item
 
-    # playlist = [...]  # Your playlist data
-
-    # Create a thread pool executor with a maximum of 5 threads
-    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
-        # Submit each item from the playlist to the executor
+    with concurrent.futures.ThreadPoolExecutor(max_workers=24) as executor:
         future_to_item = {executor.submit(process_item, item): item for item in playlist}
 
-        # Process the results as they complete
         for future in concurrent.futures.as_completed(future_to_item):
             item = future_to_item[future]
             try:
@@ -104,12 +92,11 @@ if __name__ == '__main__':
                 print(f"An error occurred while processing {item}: {e}")
             else:
                 if result is not None:
-                    # Update the playlist with the processed item
                     index = playlist.index(item)
                     print(index)
                     playlist[index] = result
 
-    print(playlist, len(playlist))
+    # print(playlist, len(playlist))
 
     with open("playlist.csv", "a", encoding="utf-8", newline="") as csvfile:
         writer = csv.writer(csvfile, delimiter=',')
@@ -119,4 +106,5 @@ if __name__ == '__main__':
             else:
                 removed_songs.append(item)
         csvfile.close()
-    print(removed_songs)
+    # print(removed_songs)
+    pass
