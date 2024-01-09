0d3af6673b6f580cc528eb280fa304f65504df3f
kusti420 <kristjan0@live.com>
2024-01-09 22:08:58 +0200

    idk

--- CSV/missing.py
+++ CSV/missing.py
@@ -1,12 +1,18 @@
 import os
-path = "K:\MEGAsync\Music\LosslessBest\sorted"
+path = "K:\MEGAsync\Music\LosslessBest\Lossless"
+path2 = "K:\MEGAsync\Music\LosslessBest\Lossy"
 
 files = os.listdir(path)
 files = [int(f.split(".")[0]) for f in files if f.split(".")[0].isdigit()]
 # print(files)
 
+files2 = os.listdir(path2)
+files2 = [int(f.split(".")[0]) for f in files2 if f.split(".")[0].isdigit()]
+
 not_in_list = []
 for i in range(1, max(files)):
     if i not in files:
-        not_in_list.append(i)
+        if i not in files2:
+            not_in_list.append(i)
+
 print(not_in_list)
--- test/test.py
+++ test/test.py
@@ -1,11 +1,23 @@
+"""shift back if some songs removed from playlist"""
 import os
-files = os.listdir("K:\MEGAsync\Music\LosslessBest\sorted")
-files.remove("desktop.ini")
-print(files)
+# path = "K:\MEGAsync\Music\LosslessBest\Lossless"
+# path = "K:\MEGAsync\Music\LosslessBest\Lossy"
+def shift_back(path):
+    files = os.listdir(path)
+    if "desktop.ini" in files:
+        files.remove("desktop.ini")
 
-for file in files:
-    index, file = int(file.split(".")[0]), file[len(str(int(file.split(".")[0]))) + 1:]
-    # print(index, file)
-    if index >= 4654 and index <= 4698:
-        os.rename(f"K:\MEGAsync\Music\LosslessBest\sorted\{index}.{file}", f"K:\MEGAsync\Music\LosslessBest\sorted\{index - 1}.{file}")
-        print(index)
+    # print(files)
+
+    for file in files:
+        index, file = int(file.split(".")[0]), file[len(str(int(file.split(".")[0]))) + 1:]
+        # print(index, file)
+        if index >= 4394 and index <= 5000:
+            try:
+                os.rename(f"{path}\{index}.{file}", f"{path}\{index - 1}.{file}")
+                print(index)
+            except:
+                pass
+
+shift_back(path = "K:\MEGAsync\Music\LosslessBest\Lossless")
+shift_back(path = "K:\MEGAsync\Music\LosslessBest\Lossy")
